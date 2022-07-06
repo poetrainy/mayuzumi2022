f7bfc409622a9ed424ca48a6b60f6ac6243fa83f
manami-sato <manamisato2001@gmail.com>
2022-07-06 13:46:15 +0900

    feat: Googleフォントの設定

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import { ColorModeScript } from '@chakra-ui/react';
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <ColorModeScript />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
