b84289ba5299917a30eff92e9bc6027bdd2f1765
manami-sato <manamisato2001@gmail.com>
2022-07-07 20:57:03 +0900

    commit

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -15,7 +15,6 @@ const Head: FC = () => {
           color="white"
           bg="primary"
           fontSize="1.4rem"
-          fontFamily="accentJa"
           p="0 0 16px"
         >
           黛灰 7月配信スケジュール帳
